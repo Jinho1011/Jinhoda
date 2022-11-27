0a04adb16264b4a4f04ce031970a95f1374aeffc
jinho1011 <gino9940@gmail.com>
2022-11-28 01:50:13 +0900

    feat: add Container and Buttons

---
+++ src/components/styles.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  max-width: 1140px;
+  width: 100%;
+  height: 100%;
+  margin: auto;
+`
+
+export const Buttons = styled.ul`
+  display: flex;
+  align-items: center;
+  padding: 0;
+  margin: 0;
+`
+
+export const Button = styled.li`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  list-style: none;
+  padding: 0 8px;
+  color: #4e5968;
+`
+
+export const ButtonText = styled.a`
+  padding: 12px 10px;
+  font-size: 15px;
+  line-height: 20px;
+  color: currentColor;
+  border-radius: 8px;
+  border: 0;
+  cursor: pointer;
+  background-color: transparent;
+  text-decoration: none;
+  text-align: left;
+
+  &:hover {
+    background-color: aliceblue;
+  }
+`

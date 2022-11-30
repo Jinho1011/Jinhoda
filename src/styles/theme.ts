35273df552fc6885b4868e4b0bac11a795644334
jinho1011 <gino9940@gmail.com>
2022-11-30 18:49:39 +0900

    feat: add theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+const color = {
+  light: {},
+  dark: {},
+  // --color-primary: #3182f6;
+  // --color-text: #4e5968;
+  // --color-text-light: #8b95a1;
+  // --color-heading: #333d4b;
+  // --color-heading-black: #191f28;
+}
+
+const lightversion = {
+  background: "#fff",
+  fontPrimary: "black",
+  fontSecondary: "gray",
+  primary: "#00a0ff",
+  secondary: "#ddd",
+  hover: "#00a0ff50",
+}
+
+const theme = {
+  lightversion,
+}
+
+export default theme

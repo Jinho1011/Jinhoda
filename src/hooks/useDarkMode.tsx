f5929f206af95b0d39ff98bf3d2286a76382f673
jinho1011 <gino9940@gmail.com>
2022-12-22 16:20:45 +0900

    fix: type safe window

--- src/hooks/useDarkMode.tsx
+++ src/hooks/useDarkMode.tsx
@@ -1,17 +1,19 @@
 import React, { useState } from "react"
 
 const getIsDarkThemeInitial = () => {
-  const persistedColorPreference = window.localStorage.getItem("color-mode")
-  const hasPersistedPreference = typeof persistedColorPreference === "string"
+  if (typeof window !== "undefined") {
+    const persistedColorPreference = window.localStorage.getItem("color-mode")
+    const hasPersistedPreference = typeof persistedColorPreference === "string"
 
-  if (hasPersistedPreference) {
-    return persistedColorPreference === "dark"
-  }
+    if (hasPersistedPreference) {
+      return persistedColorPreference === "dark"
+    }
 
-  const mql = window.matchMedia("(prefers-color-scheme: dark)")
-  const hasMediaQueryPreference = typeof mql.matches === "boolean"
-  if (hasMediaQueryPreference) {
-    return mql.matches
+    const mql = window.matchMedia("(prefers-color-scheme: dark)")
+    const hasMediaQueryPreference = typeof mql.matches === "boolean"
+    if (hasMediaQueryPreference) {
+      return mql.matches
+    }
   }
 
   return false

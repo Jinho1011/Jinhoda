16cbf7c2e1853d52900bb82d24b25c23935b9d69
jinho1011 <gino9940@gmail.com>
2023-01-02 00:04:57 +0900

    fix: add initialIsDarkMode

--- src/components/StyledThemeProvider.tsx
+++ src/components/StyledThemeProvider.tsx
@@ -1,11 +1,19 @@
 import React, { useContext, useEffect } from "react"
 import { ThemeProvider } from "styled-components"
 import DarkThemeContext from "../context/DarkTheme"
+import { getIsDarkThemeInitial } from "../hooks/useDarkMode"
 import { darkTheme, lightTheme } from "../styles/theme"
 
 const StyledThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const initialIsDarkMode = getIsDarkThemeInitial()
   const { isDarkMode } = useContext(DarkThemeContext)
   const theme = isDarkMode ? darkTheme : lightTheme
+
+  useEffect(() => {
+    document.body.style.backgroundColor = initialIsDarkMode
+      ? "#18171C"
+      : "#ffffff"
+  }, [])
 
   useEffect(() => {
     document.body.style.backgroundColor = isDarkMode ? "#18171C" : "#ffffff"

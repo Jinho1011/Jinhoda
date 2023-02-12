4e2d8d57513f3f38a05741331b0d39815da26ecf
Jeon <gino9940@gmail.com>
2023-02-12 20:52:15 +0900

    refactor: move css to assets/styles

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,9 +1,9 @@
 import React from "react"
 
 // normalize CSS across browsers
-import "./src/normalize.css"
+import "./src/assets/styles//normalize.css"
 // custom CSS styles
-import "./src/style.css"
+import "./src/assets/styles/style.css"
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
 
